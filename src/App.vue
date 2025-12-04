b5e90e91916425911f272855a85ce0b66d570f6b
MarkDeSutter <desu5994@stthomas.edu>
2025-12-03 21:14:04 -0600

    added todo and created table and markers on map

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { reactive, ref, onMounted } from 'vue'
 
 let crime_url = ref('');
 let dialog_err = ref(false);
+
+let incidents = ref([]);
+let neighborhoods = ref([]);
+let crime_counts = reactive({});
 let map = reactive(
     {
         leaflet: null,
@@ -17,23 +21,23 @@ let map = reactive(
             se: {lat: 44.883658, lng: -92.993787}
         },
         neighborhood_markers: [
-            {location: [44.942068, -93.020521], marker: null},
-            {location: [44.977413, -93.025156], marker: null},
-            {location: [44.931244, -93.079578], marker: null},
-            {location: [44.956192, -93.060189], marker: null},
-            {location: [44.978883, -93.068163], marker: null},
-            {location: [44.975766, -93.113887], marker: null},
-            {location: [44.959639, -93.121271], marker: null},
-            {location: [44.947700, -93.128505], marker: null},
-            {location: [44.930276, -93.119911], marker: null},
-            {location: [44.982752, -93.147910], marker: null},
-            {location: [44.963631, -93.167548], marker: null},
-            {location: [44.973971, -93.197965], marker: null},
-            {location: [44.949043, -93.178261], marker: null},
-            {location: [44.934848, -93.176736], marker: null},
-            {location: [44.913106, -93.170779], marker: null},
-            {location: [44.937705, -93.136997], marker: null},
-            {location: [44.949203, -93.093739], marker: null}
+            {location: [44.942068, -93.020521], marker: null, name: "Conway/Battlecreek/Highwood"},
+            {location: [44.977413, -93.025156], marker: null, name: "Greater East Side"},
+            {location: [44.931244, -93.079578], marker: null, name: "West Side"},
+            {location: [44.956192, -93.060189], marker: null, name: "Dayton's Bluff"},
+            {location: [44.978883, -93.068163], marker: null, name: "Payne/Phalen"},
+            {location: [44.975766, -93.113887], marker: null, name: "North End"},
+            {location: [44.959639, -93.121271], marker: null, name: "Thomas/Dale(Frogtown)"},
+            {location: [44.947700, -93.128505], marker: null, name: "Summit/University"},
+            {location: [44.930276, -93.119911], marker: null, name: "West Seventh"},
+            {location: [44.982752, -93.147910], marker: null, name: "Como"},
+            {location: [44.963631, -93.167548], marker: null, name: "Hamline/Midway"},
+            {location: [44.973971, -93.197965], marker: null, name: "St. Anthony"},
+            {location: [44.949043, -93.178261], marker: null, name: "Union Park"},
+            {location: [44.934848, -93.176736], marker: null, name: "Macalester-Groveland"},
+            {location: [44.913106, -93.170779], marker: null, name: "Highland"},
+            {location: [44.937705, -93.136997], marker: null, name: "Summit Hill"},
+            {location: [44.949203, -93.093739], marker: null, name: "Capitol River"}
         ]
     }
 );
@@ -69,10 +73,62 @@ onMounted(() => {
 
 // FUNCTIONS
 // Function called once user has entered REST API URL
-function initializeCrimes() {
+async function initializeCrimes() {
     // TODO: get code and neighborhood data
     //       get initial 1000 crimes
+
+    console.log("Crime URL:" + crime_url.value);
+
+    //uses await to asynchronously build 2 different arrays, one for the incidents and the other for the neighborhood names
+    try {
+        let incidents_response = await fetch(crime_url.value + '/incidents', { method: 'GET' });
+        let incident_data = await incidents_response.json();
+        console.log(incident_data);
+        incidents.value = incident_data;
+
+        let neighborhood_names_map = {};
+        let neighborhood_response = await fetch(crime_url.value + '/neighborhoods', { method: 'GET'});
+        let neighborhood_data = await neighborhood_response.json();
+        for(let i = 0; i < neighborhood_data.neighborhoods.length; i++){
+            let id = neighborhood_data.neighborhoods[i].neighborhood_number;
+            let name = neighborhood_data.neighborhoods[i].neighborhood_name;
+            neighborhood_names_map[id] = name;
+        }
+        //console.log(neighborhood_names_map);
+
+        if(incidents.value.length > 0){
+            for(let i = 0; i < incidents.value.length; i++){
+                incidents.value[i].neighborhood = neighborhood_names_map[incidents.value[i].neighborhood_number];
+                if(!(incidents.value[i].neighborhood in crime_counts)){
+                    crime_counts[incidents.value[i].neighborhood] = 1;
+                }
+                else{
+                    crime_counts[incidents.value[i].neighborhood] = crime_counts[incidents.value[i].neighborhood] + 1;
+                }
+            }
+            
+            console.log(crime_counts);
+            updateMarkerTitles();
+        }
+
+        console.log(incidents);
+
+    }
+    catch (err) {
+        console.error('Failed to load incidents or neighborhoods:', err);
+    }
 }
+
+// Update marker titles with crime counts after incidents are loaded
+function updateMarkerTitles() {
+    for(let i = 0; i < map.neighborhood_markers.length; i++){
+        let name = map.neighborhood_markers[i].name;
+        let crime_count = crime_counts[name];
+        map.neighborhood_markers[i].marker = L.marker(map.neighborhood_markers[i].location, { title: name  + '\nCrimes: ' + crime_count}).addTo(map.leaflet);
+        map.neighborhood_markers[i].marker.bindPopup(name);
+    }
+}
+
 
 // Function called when user presses 'OK' on dialog box
 function closeDialog() {
@@ -102,6 +158,31 @@ function closeDialog() {
         <div class="grid-x grid-padding-x">
             <div id="leafletmap" class="cell auto"></div>
         </div>
+    </div>
+
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <td>Case Number</td>
+                    <td>Date</td>
+                    <td>Incident</td>
+                    <td>Police Grid #</td>
+                    <td>Neighborhood</td>
+                    <td>Block</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(incident, index) in incidents">
+                    <td>{{ incident.case_number }}</td>
+                    <td>{{ incident.date_time }}</td>
+                    <td>{{ incident.incident }}</td>
+                    <td>{{ incident.police_grid }}</td>
+                    <td>{{ incident.neighborhood }}</td>
+                    <td>{{ incident.block }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
